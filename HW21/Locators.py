e8f3855b55652043f3718c7e1e5f431383bc7624
AndriyPovzun <povzuun99@gmail.com>
2022-11-10 18:41:25 +0200

    Write_all_locators

--- HW21/Locators.py
+++ HW21/Locators.py
@@ -1,3 +1,40 @@
-"""
-JUST TEST
-"""
+# Locators for website https://veliki.com.ua/ua/
+veliki_heading_bicycle_locator = "//img[@alt='Велосипеди']"
+veliki_search_button_locator = "//div[@class='butt-search']"
+veliki_menu_button_locator = "//span[@class='butt-menu']"
+veliki_brands_button_locator = "//a[text()='бренди']"
+veliki_favorite_button_locator = "//div[@class='wl']"
+css_cart_button_locator = '[class="cart"]'
+
+# Locators for website https://auto.ria.com/uk/
+sell_car_button_locator = "//div[@class='areabar']//a[@class='button-add']"
+my_profile_button_locator = "//span[@class='tl']"
+support_ZSU_button_locator = "//nav[@class='nav-head']//a[last()]"
+new_auto_button_locator = "//div//a[@data-type='new']"
+search_button_locator = "//button[@type='submit']"
+chat_button_locator = "//div[@class='app-head-bar']//a[@id='chatRia']"
+advanced_search_button_locator = "//a[@class='ext-end']"
+css_autoria_instagram_button_locator = "[class='item instagram']"
+
+# Locators for website https://www.olx.ua/uk/
+olx_add_new_ad_locator = "//a[@id='postNewAdLink']"
+olx_link_on_appstore_app_button_locator = "//a[@id='footerAppIphone']"
+olx_search_button_locator = "//input[@id='submit-searchmain']"
+css_auto_category_button_locator = '[data-id="1532"][class="link parent   "]'
+
+# Locators for website https://dou.ua/
+css_dou_login_button_locator = '[id="login-link"]'
+css_dou_main_page_button_locator = '[class="logo"]'
+css_dou_search_field_locator = "[name='q']"
+css_dou_relocate_section_button_locator = '[title="Relocate"]'
+
+# Locators for website https://store.steampowered.com
+css_steam_install_button_locator = '[class="header_installsteam_btn_content"]'
+css_steam_search_field_locator = "[id='store_nav_search_term']"
+css_steam_login_button_locator = '[class="global_action_link"]'
+css_steam_change_language_button_locator = "[id='language_pulldown']"
+# Locators for website https://jabko.ua/
+css_jabko_support_button_locator = "bingc-phone-button-icon-icon"
+css_jabko_search_field_locator = '[method="GET"]'
+css_jabko_radio_button_locator = '[class="hash_links radio"]'
+css_send_email_massage_locator = '[class="phone1"]'
