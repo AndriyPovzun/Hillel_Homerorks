08c46a00b951ca12381f8d6ad8196552e62a325a
AndriyPovzun <povzuun99@gmail.com>
2022-11-12 15:17:21 +0200

    Write_all_locators

--- HW21/Locators.py
+++ HW21/Locators.py
@@ -1,40 +1,49 @@
 # Locators for website https://veliki.com.ua/ua/
-veliki_heading_bicycle_locator = "//img[@alt='Велосипеди']"
-veliki_search_button_locator = "//div[@class='butt-search']"
-veliki_menu_button_locator = "//span[@class='butt-menu']"
-veliki_brands_button_locator = "//a[text()='бренди']"
-veliki_favorite_button_locator = "//div[@class='wl']"
+category_bicycle = "//img[@alt='Велосипеди']"
+search_button = "//div[@class='butt-search']"
+menu_button = "//span[@class='butt-menu']"
+search_category_field = '//div[@class="menu-search"]//label[@class="textinput"]'
+gifts_button = '//li[@data-id="9021"]'
+favorite_button = "//div[@class='wl']"
+login_button = '//div[@class="client notentered"]'
+subscribe_for_promotion_field = '//div[@class="subscribe"]//label'
+subscribe_for_promotion_submit_button = '//div[@class="subscribe"]//button'
+instagram_store_button = '//div[@class="socials"]//span[@class="anchor in"]'
+new_buyer_registration_button = '//div[@class="client-bbody"]//span[last() -1]//input'
+surname_registration_field = "//input[@name='surname']"
+name_registration_field = "//input[@name='firstname']"
+middle_name_registration_field = "//input[@name='patronymic']"
+email_registration_field = "//input[@name='email'][@placeholder='mail@mail.ua']"
+phone_number_registration_field = '//input[@name="phone"]'
+setup_password_button = '//div[@class="clientpasslink"]//span'
+password_registration_field = "//div[@class='clientpass']//input[@name='pass']"
+submit_password_registration_field = "//div[@class='clientpass']//input[@name='pass2']"
+submit_registration_button = "//button[@class='butt4']"
+show_numbers_button = '//div[@class="botline"]//div[@class="phone"]//span'
+regular_customer_page_button = '//div[@class="clienttab"][last()]'
+regular_customer_email_login_field = "//input[@name='email'][@class='noajax']"
+regular_customer_password_login_field = '//div[@class="cliententer"]//input[@type="password"]'
+regular_customer_remember_me_checkbox = '//input[@type="checkbox"]'
+regular_customer_login_button = '//button[@id="butt-enter"]'
+regular_customer_forget_pass_button = '//span[@class="remember"]'
+black_friday_discount_button = '//div[@align="center"]//a'
+email_login_field = '//div[@id="enter-layer"]//input[@name="email"]'
+password_login_field = '//div[@id="enter-layer"]//input[@name="pass"]'
+submit_login_button = '//div[@id="enter-layer"]//button'
+
+css_electro_bike_category = 'li[data-filter="biketype:electro"]'
+css_hybrid_bike_category = 'li[data-filter="biketype:hybrid_cross"]'
 css_cart_button_locator = '[class="cart"]'
-
-# Locators for website https://auto.ria.com/uk/
-sell_car_button_locator = "//div[@class='areabar']//a[@class='button-add']"
-my_profile_button_locator = "//span[@class='tl']"
-support_ZSU_button_locator = "//nav[@class='nav-head']//a[last()]"
-new_auto_button_locator = "//div//a[@data-type='new']"
-search_button_locator = "//button[@type='submit']"
-chat_button_locator = "//div[@class='app-head-bar']//a[@id='chatRia']"
-advanced_search_button_locator = "//a[@class='ext-end']"
-css_autoria_instagram_button_locator = "[class='item instagram']"
-
-# Locators for website https://www.olx.ua/uk/
-olx_add_new_ad_locator = "//a[@id='postNewAdLink']"
-olx_link_on_appstore_app_button_locator = "//a[@id='footerAppIphone']"
-olx_search_button_locator = "//input[@id='submit-searchmain']"
-css_auto_category_button_locator = '[data-id="1532"][class="link parent   "]'
-
-# Locators for website https://dou.ua/
-css_dou_login_button_locator = '[id="login-link"]'
-css_dou_main_page_button_locator = '[class="logo"]'
-css_dou_search_field_locator = "[name='q']"
-css_dou_relocate_section_button_locator = '[title="Relocate"]'
-
-# Locators for website https://store.steampowered.com
-css_steam_install_button_locator = '[class="header_installsteam_btn_content"]'
-css_steam_search_field_locator = "[id='store_nav_search_term']"
-css_steam_login_button_locator = '[class="global_action_link"]'
-css_steam_change_language_button_locator = "[id='language_pulldown']"
-# Locators for website https://jabko.ua/
-css_jabko_support_button_locator = "bingc-phone-button-icon-icon"
-css_jabko_search_field_locator = '[method="GET"]'
-css_jabko_radio_button_locator = '[class="hash_links radio"]'
-css_send_email_massage_locator = '[class="phone1"]'
+css_go_to_registration_page_button = 'a[class="butt2"]'
+css_remember_me_login_button = 'div[id="enter-layer"] span[data-callback]'
+css_compare_prices_button = 'div[class="cmp"]'
+css_change_language = 'div[class="lang"] a'
+css_base_page = 'a[class="logo"]'
+# Recommendation bike constructor
+css_height_field = 'span[data-build="BikeSelectGrowthBuild"]'
+css_weight_field = 'span[data-build="BikeSelectWeightBuild"]'
+css_i_will_ride_on_selector = 'span[data-callback="BikeSelectRaodSelect"]'
+css_style_bike_selector = 'span[data-callback="BikeSelectStyleSelect"]'
+css_price_selector = 'data-callback="BikeSelectPriceSelect"'
+css_find_bike_button = 'div[class="field"] span[class="butt1"]'
+css_more_detail_about_model_button = 'div[class="info"]  a[class="butt"]'
