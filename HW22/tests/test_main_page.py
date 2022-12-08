ddea57d6565da0247b91678eae403c64c5b20bb8
AndriyPovzun <povzuun99@gmail.com>
2022-12-08 21:49:30 +0200

    tests for main page

--- HW22/tests/test_main_page.py
+++ HW22/tests/test_main_page.py
@@ -3,12 +3,6 @@ def test_login(login):
     assert personal_cabinet_page.is_logout_button_displayed() is True, 'User not logged-in'
 
 
-# def test_subscribe_on_promotion(open_main_page, env):
-#     main_page = open_main_page
-#     main_page.subscribe_on_promotions(env.user_email)
-#     assert main_page.subscribe_success_is_displayed() is True, 'User not was subscribed on promotions'
-
-
 def test_change_language(open_main_page):
     main_page = open_main_page
     main_page.change_page_language()
@@ -21,3 +15,15 @@ def test_bike_recommendation_constructor(scroll_to_bike_constructor):
     main_page.fill_constructor()
     assert bike_before_constructor != main_page.get_bike_name_in_constructor(), 'Constructor does\'t work'
 
+
+def test_check_store_number_droptable(open_main_page):
+    main_page = open_main_page
+    main_page.click_show_numbers()
+    assert main_page.is_open_numbers_droptable() is True, 'Table was not opened'
+
+
+def test_search_button(open_main_page):
+    main_page = open_main_page
+    assert main_page.is_search_field_closed() is True, 'Search field not closed'
+    main_page.click_search_button()
+    assert main_page.is_search_field_opened() is True, 'Search field not opened'
